16f3b7e198de99cbafc51cb9d3e8ca679ebcbe0d
DevMatheusMonteiro <carlosmathmonteiro@gmail.com>
2024-07-28 10:11:06 -0300

    creating service and controller

---
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package me.dio.service.impl;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User create(User user) {
+        if (userRepository.existsByAccountNumber(user.getAccount().getNumber())) {
+            throw new IllegalArgumentException("User with account number " + user.getAccount().getNumber() + " already exists");
+        }
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findById(int id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+}
